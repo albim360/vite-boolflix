6fbb4021ee6df92028efc83a1d81049b5d27e312
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-08 16:54:53 +0100

    FIX bandiera inglese/americana

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,8 +4,8 @@
         <li v-for="media in mediaList" :key="media.id">
           <h2>{{ media.media_type === 'tv' ? media.name : media.title }}</h2>
           <p><strong>Titolo originale:</strong> {{ media.media_type === 'tv' ? media.original_name : media.original_title }}</p>
-          <p v-if="media.media_type === 'movie'"><strong>Lingua:</strong> <span :class="`flag-icon flag-icon-${media.original_language}`"></span></p>
-          <p v-if="media.media_type === 'tv'"><strong>Lingua:</strong> <span :class="`flag-icon flag-icon-${media.original_language}`"></span></p>          
+          <p v-if="media.media_type === 'movie'"><strong>Lingua:</strong> <span :class="`fi fi-${media.original_language}`"></span></p>
+          <p v-if="media.media_type === 'tv'"><strong>Lingua:</strong> <span :class="`fi fi-${media.original_language}`"></span></p>          
           <p><strong>Voto:</strong> {{ media.vote_average }}</p>
         </li>
       </ul>

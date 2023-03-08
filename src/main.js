3357d845484440baacf1d5540fd91a977765547e
Albim360 <alberto.zappala@icloud.com>
2023-03-08 16:06:19 +0100

    FEAT aggiunte bandiere

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
-import '../style/general.scss'
 import App from './App.vue'
+import '../style/general.scss'
+import '../node_modules/flag-icon-css/css/flag-icons.min.css'
 
 createApp(App).mount('#app')

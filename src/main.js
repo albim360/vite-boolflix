a4533ae8d5f07b22d3f62affe85951f444bf2e25
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-09 16:14:52 +0100

    FEATT fontawesome + stelle per voto

--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import '../style/general.scss'
-import '../node_modules/flag-icon-css/css/flag-icons.min.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faStar as faSolidStar } from '@fortawesome/free-solid-svg-icons';
+import { faStar as faRegularStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import '@fortawesome/fontawesome-free/css/all.css';
+import '../style/general.scss';
+import '../node_modules/flag-icon-css/css/flag-icons.min.css';
 
-createApp(App).mount('#app')
+// Aggiungi le icone alla libreria FontAwsome
+library.add(faSolidStar, faRegularStar);
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app');

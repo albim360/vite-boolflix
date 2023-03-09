4ede33d1aedc7fe3f6a3ceb8c9bbfd66837aeb22
Alberto <74522184+albim360@users.noreply.github.com>
2023-03-09 16:16:49 +0100

    FEAT aggiunge immagini copertina

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -3,14 +3,11 @@
     <ul>
       <li v-for="media in mediaList" :key="media.id">
         <h2>{{ media.media_type === 'tv' ? media.name : media.title }}</h2>
+        <img :src="'https://image.tmdb.org/t/p/w342/' + media.poster_path" alt="">
         <p><strong>Titolo originale:</strong> {{ media.media_type === 'tv' ? media.original_name : media.original_title }}</p>
         <p v-if="media.media_type === 'movie'"><strong>Lingua:</strong> <span :class="`fi fi-${media.original_language}`"></span></p>
         <p v-if="media.media_type === 'tv'"><strong>Lingua:</strong> <span :class="`fi fi-${media.original_language}`"></span></p>          
-        <div class="star-rating">
-          <template v-for="star in starList(media.vote_average)">
-            <i :class="[star ? 'fas' : 'far', 'fa-star']"></i>
-          </template>
-        </div>
+        <p><strong>Voto:</strong> {{ media.vote_average }}</p>
       </li>
     </ul>
   </main>
